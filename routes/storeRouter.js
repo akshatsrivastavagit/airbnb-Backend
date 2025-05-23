3a40af8efb04691e87eb39f93f6939eb912ee562
akshatgithubgre <akshat201103@gmail.com>
2025-05-23 18:33:26 +0530

    final commit

--- routes/storeRouter.js
+++ routes/storeRouter.js
@@ -1,18 +1,17 @@
-//External router
+// External Module
 const express = require("express");
 const storeRouter = express.Router();
-//express will make a router for us so that we can use this user router in the app.js
 
+// Local Module
 const storeController = require("../controllers/storeController");
 
 storeRouter.get("/", storeController.getIndex);
 storeRouter.get("/homes", storeController.getHomes);
 storeRouter.get("/bookings", storeController.getBookings);
 storeRouter.get("/favourites", storeController.getFavouriteList);
+
+storeRouter.get("/homes/:homeId", storeController.getHomeDetails);
 storeRouter.post("/favourites", storeController.postAddToFavourite);
+storeRouter.post("/favourites/delete/:homeId", storeController.postRemoveFromFavourite);
 
-storeRouter.get("/homes/:homeId",storeController.getHomeDetails)
-
-
-storeRouter.post("/favourites/delete/:homeId",storeController.postRemoveFromFavourite)
 module.exports = storeRouter;
